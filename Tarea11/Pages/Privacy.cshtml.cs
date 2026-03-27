3d20dc1955d7e10eb50ad7f668666b2ee02c1135
Gabriel P. <gabrielpena0209@gmail.com>
2026-03-26 22:47:00 -0400

    first commit

---
+++ Tarea11/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarea11.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
